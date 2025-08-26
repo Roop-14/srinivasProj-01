8f0962f5338568f853268e47f316bbb71ff7ecc3
Roopam <roopammondal14@gmail.com>
2025-08-26 21:48:11 +0530

    26/8/25:projects ammendement

--- booking-service/src/main/java/com/movie/ticket/booking/brokers/PaymentServiceBroker.java
+++ booking-service/src/main/java/com/movie/ticket/booking/brokers/PaymentServiceBroker.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "Payment-service" ,url = "http://localhost:9091/payments")
+@FeignClient(name = "Payment-service" )
 public interface PaymentServiceBroker {
 
 
